659f338c4ec1926b3fa537877ec949254dc8215a
LewengLekodietla <lewengm@icloud.com>
2024-03-06 13:46:59 +0200

    Initial commit

---
+++ Restaurant.java
@@ -0,0 +1,19 @@
+package CapstoneProjectOOP;
+
+public class Restaurant {
+	
+	private String name;
+    private String area;
+
+    public Restaurant(String name, String area) {
+        this.area = name;
+        this.area = area;
+    }
+    public String getName() {
+    	return name;
+    }
+
+    public String getArea() {
+        return area;
+    }
+}
